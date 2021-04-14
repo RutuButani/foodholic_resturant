7876f783f3cf4e9f6cc0ccfcada36784f9e4a45b
RutuButani <80757931+RutuButani@users.noreply.github.com>
2021-04-14 10:27:18 +0530

    Add files via upload

--- php/contact.php
+++ php/contact.php
@@ -1,1 +1,22 @@
+<?php
 
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$massage=$_POST['massage'];
+
+$servername="localhost:3306";
+$username="root";
+$password="root";
+$dbname="foodholic";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+
+echo "Connected Successully";
+
+$sqlquery="INSERT INTO contact_table (first_name,last_name,email,massage) VALUES ('$fname','$lname','$email','$massage')";
+
+$conn->query($sqlquery);
+
+?>
