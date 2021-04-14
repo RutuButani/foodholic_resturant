50e4fae677c109f56fadf5a4c85144490d11a087
RutuButani <80757931+RutuButani@users.noreply.github.com>
2021-04-14 10:26:42 +0530

    Add files via upload

--- php/book_table.php
+++ php/book_table.php
@@ -1,1 +1,25 @@
+<?php
 
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$email=$_POST['email'];
+$adress=$_POST['adress'];
+$nperson=$_POST['nperson'];
+$date=$_POST['date'];
+$timing=$_POST['timing'];
+$tableno=$_POST['tableno'];
+
+$servername="localhost:3306";
+$username="root";
+$password="root";
+$dbname="foodholic";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+echo "Connected Successully";
+
+$sqlquery="INSERT INTO registration_table (first_name,last_name,email,number_of_person,visit_time,visit_date,table_number) VALUES ('fname','lname','email','adress','nperson','date','timing','tableno')";
+
+$conn->query($sqlquery);
+
+?>
